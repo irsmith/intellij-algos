e373f281dc089819a191daf5c6b8af908a911053
issmith1 <irene.s.smith@nasa.gov>
2016-04-03 20:02:20 -0700

    assignment permit is n factorial

--- src/main/java/recursion/Permute.java
+++ src/main/java/recursion/Permute.java
@@ -7,31 +7,40 @@ public class Permute {
 
 
     public static void main(String[] args) {
-        char[] a = new char[]{'a','b'};
+        char[] a = new char[]{'a','b','c','d'};
 
-        perm(a, 0, a.length-1, 0);
+        perm(a, 0);  //start at zero, swap 0w0, 0w1,0w2,0w3 recurse on zero+1
+        System.out.println("--------");
+
+        permChunks("", new String(a));
     }
 
-    private static void perm(char[] a, int start, int end, int depth) {
+    private static void permChunks(String s, String a) {
+        // accumulate answer in s. Split a into 2 chunks
+        if (a.length() == 1) {
+            System.out.println(s+a);
+        } else {
+            for (int i = 0;i<a.length();i++) {
+                permChunks(s+a.charAt(i),
+                        a.substring(0, i) + a.substring(i+1));
+            }
+        }
+    }
 
-        depth++;
-        indent(depth);
-        System.out.format("depth:%d start:%d end:%d %n",depth,start,end);
-        flush();
+    private static void perm(char[] a, int start) {
+
+        //System.out.format("start:%d  %n",start);
 
         if (start == a.length -1) {
-            // print since length is zero
-            indent(depth);
             System.out.println(a);
             flush();
         } else {
             for (int i=start;i < a.length;i++ ) {
-                swap(a, i, end);
-                perm(a, i+1, end, depth);
+                swap(a, i, start);
+                perm(a, start+1);
                 swap(a, i, start);
             }
         }
-        depth--;
     }
 
     private static void flush() {
