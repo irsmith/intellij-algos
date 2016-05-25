372c4dea6f2af943c8b042eebd9643afdacb0594
Smith, Irene S. (ARC-TI)[SGT, INC] <irene.s.smith@nasa.gov>
2016-05-25 11:47:49 -0700

    shuffle even-probability solution, reverse linked list, with explanation

--- src/main/java/array/Shuffle.java
+++ src/main/java/array/Shuffle.java
@@ -1,7 +1,27 @@
 package array;
 
+import java.util.Arrays;
+import java.util.Random;
+
 /**
  * Created by issmith1 on 5/21/16.
  */
 public class Shuffle {
+    public static void main(String[] args) {
+        // equally likely
+        Random r = new Random();
+        Integer a[] = {0, 1, 2, 3, 4, 5};
+
+        for (int i = a.length - 1; i > 0; i--) {
+            int idx = r.nextInt(i);
+            swap(a, i, idx);
+        }
+        System.out.println(Arrays.asList(a));
+    }
+
+    private static void swap(Integer[] a, int i, int j) {
+        int tmp = a[i];
+        a[i] = a[j];
+        a[j] = tmp;
+    }
 }
--- src/main/java/linkedList/ReverseLL.java
+++ src/main/java/linkedList/ReverseLL.java
@@ -2,31 +2,68 @@ package linkedList;
 
 import linkedList.LinkListUtil.Node;
 
+/*
+clarifying q:
+   will I be given null list?
+
+ */
 public class ReverseLL {
 	Node h;
 
 	public static void main(String[] args) {
 
-		Node nc = new Node('c', null);
-		Node nb = new Node('b', nc);
-		Node na = new Node('a', nb);
-		LinkListUtil.print(na);
+		Node nr = new Node('r', null);
+		Node ne = new Node('e', nr);
+		Node nt = new Node('t', ne);
+		Node na = new Node('a', nt);
+		Node nw = new Node('w', na);
 
-		Node newhead = rev(na);
+		LinkListUtil.print(nw);
+		Node newhead = null;
+
+//		newhead = rev(nw);
+//		LinkListUtil.print(newhead);
+
+		newhead = rec_rev(nw);
 		LinkListUtil.print(newhead);
 
+
 	}
 
-	private static Node rev(Node cur) {
-		if (cur.nxt == null) {
-			return cur; // new head
+	//recursive
+	private static Node rec_rev(Node cur) {
+
+		//if (cur==null) return null; //debug:: no because I append to second list
+		if (cur.nxt==null) return cur; //fix
+
+		Node secondList = cur.nxt; //its first element before the call
+		Node attachSingleChar = cur; // dont need attachSingleChar (could use cur) but it helps me learn
+		attachSingleChar.nxt = null;
+
+		Node newhead = rec_rev(secondList);
+
+		// edge is last element of second list after the call
+		secondList.nxt = attachSingleChar;
+		//cur.nxt = null;  // result if here is "r w" for 'water'  BUG!
+		return newhead;
+	}
+
+	private static Node rev(Node head) {
+		Node cur = head;
+		Node prev = null;
+		Node newHead = null;
+		while (cur != null) {
+
+			Node save = cur.nxt;
+			cur.nxt = prev;
+			newHead = cur;
+			// increment
+			prev = cur;  //example of debug::other order is wrong
+			cur = save;  //example of debug::
+
 		}
-
-		Node second = cur.nxt; 
-		Node revOfSecond = rev(second);
-		Node lastOfSecondList = second;
-		lastOfSecondList.nxt = cur; // attach cur to end of reverse of second
-		cur.nxt = null;
-		return revOfSecond;
+		return newHead;
 	}
+
+
 }
