2b626379f9b6253da087f4a9e6261d60b58e9894
Smith, Irene S. (ARC-TI)[SGT, INC] <irene.s.smith@nasa.gov>
2016-09-28 11:27:57 -0700

    IAP in september 2016

---
+++ src/main/java/strings/Palidrone.java
@@ -0,0 +1,6 @@
+package strings;
+
+//Determine if a given string is a palindrome. A palindrome is a string that is the same forwards as backwards, ignoring letter case, punctuation marks and word dividers. For example, “racecar” and “Amor, Roma” are palindromes, “abcd” is not. We limit the punctuation marks to ignore by the period, comma, question mark, exclamation point, apostrophe, colon, semi colon and hyphen.
+
+public class Palidrone {
+}
---
+++ src/main/java/strings/PrintDuplicates.java
@@ -0,0 +1,8 @@
+package strings;
+
+/**
+ * Print duplicate elements in a given string.
+ */
+public class PrintDuplicates {
+
+}
